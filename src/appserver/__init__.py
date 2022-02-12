3e860371a1a7b380fcce174bca50e2c47c066e42
Toomas Vendelin <toomas.vendelin@gmail.com>
2022-02-12 16:41:05 +0100

    config for appserver

--- src/appserver/__init__.py
+++ src/appserver/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify, make_response
 from werkzeug.exceptions import NotFound
 from cuboid import Cuboid
 from decimal import Decimal, InvalidOperation
-
+import os
 
 def create_api(test_config=None):
     api = Flask(__name__)
+    api.config.from_envvar('CUBOID_CONFIG')
 
 
     @api.route('/v1/cuboids', methods=['GET'])
@@ -29,12 +30,15 @@ def create_api(test_config=None):
         r = make_response(jsonify(about_cuboid), 200)
         return r
 
+        return r
+        
 
     @api.errorhandler(NotFound)
     def not_found_handler(e):
         return jsonify(e.__dict__), 404
 
     return api
+
 
 def list_cuboids():
     r = make_response(jsonify(dict()), 200)
