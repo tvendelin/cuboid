742aac0ad29b845c899d954fed72512a96683b76
Toomas Vendelin <toomas.vendelin@gmail.com>
2022-02-12 13:25:29 +0100

    Moved CLI functionality to cli.py

--- src/cuboid/cli.py
+++ src/cuboid/cli.py
@@ -1,28 +1,33 @@
-if (__name__ == '__main__'):
-    import sys
-    from decimal import *
+import sys
+from decimal import Decimal, InvalidOperation
+from cuboid import Cuboid
 
-    usage_msg = '''
+usage_msg = '''
     cuboid
 
     Calculates volume, surface and perimeter of a rectangular cuboid
 
     Usage: 
     cuboid <edge a> <edge b> <edge c>
-    '''
+'''
 
-    def usage(err=''):
-        if err:
-            print(f'\n    Error:\n    {err}')
-        print(usage_msg)
-        sys.exit(1)
+def usage(err=None):
+    if err:
+        print(f'\n    Error:\n    {err}')
+    print(usage_msg)
+    sys.exit(1)
 
-    if len(sys.argv) != 4:
+
+def cli(*args):
+    if len(args) == 0:
+        usage()
+
+    if len(args) != 3:
         usage('Expecting 3 arguments')
 
     valid = list()
 
-    for a in sys.argv[1:]:
+    for a in args:
         try:
             x = Decimal(a)
             if x < 0:
@@ -39,4 +44,5 @@ if (__name__ == '__main__'):
     print(f'Perimeter: {round(c.perimeter(), 2)}')
 
 
-
+def main():
+    cli(*sys.argv[1:])
